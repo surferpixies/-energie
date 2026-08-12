49aaf3068d3ef4f4bf06aea99a9de1ecbf7a7edc
surferpixies <njc6wj785p@privaterelay.appleid.com>
2026-08-11 20:11:49 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -9162,7 +9162,7 @@
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", async () => {
       try {
-        const reg = await navigator.serviceWorker.register("./sw.js?v=3.36.8");
+        const reg = await navigator.serviceWorker.register("./sw.js?v=3.36.9");
         await reg.update();
         let refreshing = false;
         navigator.serviceWorker.addEventListener("controllerchange", () => {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "energie-runtime-v3.36.8";
+const CACHE_NAME = "energie-runtime-v3.36.9";
 
 self.addEventListener("install", () => self.skipWaiting());
 
