a7b9fe516a0ac69436c58eb89aeee733a31e3059
surferpixies <njc6wj785p@privaterelay.appleid.com>
2026-09-25 15:50:08 -0400

    Add CNF catalog runtime placeholder

---
+++ cnf-catalog.js
@@ -0,0 +1,10 @@
+(function (root) {
+  "use strict";
+  root.ENERGIE_CNF_CATALOG = Object.freeze([]);
+  root.ENERGIE_CNF_CATALOG_META = Object.freeze({
+    edition: 2026,
+    source: "Health Canada / Santé Canada",
+    foods: 0
+  });
+  if (typeof module !== "undefined" && module.exports) module.exports = root.ENERGIE_CNF_CATALOG;
+})(typeof window !== "undefined" ? window : globalThis);
